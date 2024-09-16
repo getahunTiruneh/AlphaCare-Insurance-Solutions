e5ee1c64f78f0006fffbc7a8e3b87b2460e05443
getahunTiruneh <gechtiru@gmail.com>
2024-09-16 20:59:50 +0300

    modify AB_hypotesis testing scripts

--- scripts/AB_haypothesis_tester.py
+++ scripts/AB_haypothesis_tester.py
@@ -4,18 +4,25 @@ import scipy.stats as stats
 class ABHypothesisTester:
     def __init__(self, data):
         self.data = data
-
-
     # Define control and test groups
     def create_groups(self, df, feature, group_A_values, group_B_values):
         group_A = df[df[feature].isin(group_A_values)]
         group_B = df[df[feature].isin(group_B_values)]
         return group_A, group_B
 
-    # Example: Hypothesis Testing for Gender
+    # Hypothesis Testing for Gender
     def create_gender_groups(self, df):
         group_A = df[df['Gender'] == 'Male']
         group_B = df[df['Gender'] == 'Female']
+        return group_A, group_B
+    
+    # Perform Hypothesis Testing for ZipCode
+    def create_zipcode_groups(self, df):
+        # Define a threshold or specific zip codes for grouping
+        zipcodes = df['PostalCode'].unique()
+        mid_index = len(zipcodes) // 2
+        group_A = df[df['PostalCode'].isin(zipcodes[:mid_index])]
+        group_B = df[df['PostalCode'].isin(zipcodes[mid_index:])]
         return group_A, group_B
     # Hypothesis Testing Function
     def hypothesis_test(self, group_A, group_B, column, test_type='t'):
@@ -38,4 +45,3 @@ class ABHypothesisTester:
             result = "Fail to reject the null hypothesis"
         
         print(f"{test_name}: p-value = {p_value:.4f} -> {result}")
-
